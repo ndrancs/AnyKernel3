0d01137bec105de604822d69b77e7937bd49333b
Excalibur-99 <txexcalibur99@gmail.com>
2022-05-17 17:57:08 +0530

    AK3: Bringup kernel for miatoll
    Signed-off-by: Excalibur-99 <txexcalibur99@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,20 +4,24 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=Velvet Kernel by TxExcalibur
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-supported.versions=
+device.name1=miatoll
+device.name2=excalibur
+device.name3=curtana
+device.name4=gram
+device.name5=joyeuse
+supported.versions=11.0-12.0
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
